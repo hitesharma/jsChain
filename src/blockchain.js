2bc7fddcb98e830a37e49f7e3e603407d12aae74
Hitesh Sharma <hitesharma3899@gmail.com>
2020-05-18 12:08:50 +0530

    added pending transactions

--- src/blockchain.js
+++ src/blockchain.js
@@ -37,6 +37,22 @@ class Blockchain{
         this.miningReward=1; 
     }
 
+    genesisBlock(){
+        return new Block(Date.now(),"Genesis Block","0");
+    }
+
+    minePendingTransactions(miningRewardAddress){
+        let newBlock=new Block(Date.now(),this.pendingTransactions);
+        newBlock.mineBlock(this.difficulty);
+        console.log("Block mine success");
+        this.chain.push(newBlock);
+        this.pendingTransactions=[ new Transaction(null,miningRewardAddress,this.miningReward) ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
     getLatestBlock(){
         return this.chain[this.chain.length-1];
     }
